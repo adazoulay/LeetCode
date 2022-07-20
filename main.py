67b7f206bc2d2c39b06aa7aa3301d7bb7bece085
adazoulay <adamazoulay86@gmail.com>
2022-07-20 11:05:11 -0400

    Setting up

---
+++ main.py
@@ -0,0 +1,12 @@
+def even_odd(A):
+    next_even, next_odd = 0, len(A) - 1
+    while next_even < next_odd:
+        if A[next_even] % 2 == 0:
+            next_even += 1
+        else:
+            A[next_even], A[next_odd] = A[next_odd], A[next_even]
+            next_odd -= 1
+    print(A)
+
+
+even_odd([1,2,7,5,4,3,6,])
